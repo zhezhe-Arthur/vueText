fb568c71ac75d3b2ba10a13b26cfb5df97fb9577
彭喆 <pengzhechina@sina.com>
2020-07-31 00:26:42 +0800

    计算器组件UI

--- src/components/calculator.vue
+++ src/components/calculator.vue
@@ -1,18 +1,65 @@
 <template>
     <div>
         <div class="calculatorAll">
-            计算器组件
+            <div class="displayInfo">
+                <div class="displayInput">
+                    <input type="text" class="displayInputA" disabled v-model="resultInput">
+                    <input type="text" class="displayInputB" autofocus="autofocus">
+                </div>
+                <div class="keyboardInfo">
+
+                </div>         
+            </div>
         </div>    
     </div>
 </template>
 <script>
 export default {
-    name: 'calculator'
+    name: 'calculator',
+    data() {
+        return {
+            resultInput: 0
+        }
+    },
 }
 </script>
 <style>
     .calculatorAll {
         height: 18rem;
-        background-color: #eeeaea;
+        width: 100%;
+        background-color: #dbd6d6;
+        
+    }
+    .displayInfo {
+        height: 4.25rem;
+        width: 100%;
+        background-color: #eee7e7;
+    }
+    .displayInput{
+        position: relative;
+        top: 10px;
+    }
+    
+    .displayInput>input {
+        background-color: #FFFFFF;
+        width: 80%;     
+        height: 22px;
+        border:#eee7e7;
+        text-align: right;
+    }
+    .displayInputA {
+        border-bottom: none;
+    }
+    .displayInputB {
+        border-top: none;
+    }
+    .keyboardInfo {
+        width: 80%;
+        height: 12.5rem;
+        background-color: #eee7e7;
+        position: relative;
+        top: 1.8rem;
+        /* left: 1.2rem; */
+        margin: 0 auto;
     }
 </style>
